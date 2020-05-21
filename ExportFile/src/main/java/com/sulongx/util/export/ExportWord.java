608bb9aefbd1c0cbc82749e7fb051cd8a9324ff5
Sulongx <1724641999@qq.com>
2020-05-21 11:45:44 +0800

    自定义导出工具类-Freemarker实现

--- ExportFile/src/main/java/com/sulongx/util/export/ExportWord.java
+++ ExportFile/src/main/java/com/sulongx/util/export/ExportWord.java
@@ -4,8 +4,8 @@ import freemarker.template.Configuration;
 import freemarker.template.Template;
 import freemarker.template.TemplateExceptionHandler;
 
-import java.io.File;
-import java.io.IOException;
+import javax.xml.stream.FactoryConfigurationError;
+import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -14,29 +14,74 @@ import java.util.Map;
  * @author Sulongx
  * @version 1.0
  */
-public class ExportWord<T> implements ExportAPI {
+public class ExportWord {
 
-    private T model;
-    private Map<String,T> dataMap;
+
+    private final static String templatePath;
+    private Map<String,Object> dataMap;
     private final static Configuration config;
+    private static Template template;
+
 
     //初始化config实例
     static {
+        //模板所在资源相对路径
+        templatePath = "/template/";
         config = new Configuration(Configuration.VERSION_2_3_22);
         config.setDefaultEncoding("UTF-8");
     }
 
+    private ExportWord(){}
+
     /**
-     * 构造函数-加载模板所在位置
-     * @param templatePath 模板所在目录
+     * 加载模板
+     * @param templateName 模板名
      */
-    public ExportWord(String templatePath) throws IOException {
-        config.setDirectoryForTemplateLoading(new File(templatePath));
+    public  ExportWord loadTemplate(String templateName) throws IOException{
+        config.setDirectoryForTemplateLoading(
+                new File(ExportWord.class.getResource("/").getPath()
+                        + templatePath
+                        + templateName));
         config.setTemplateExceptionHandler(TemplateExceptionHandler.RETHROW_HANDLER);
+        template = config.getTemplate(templateName);
+        return this;
     }
 
-    public <M> Boolean createNewByTemplate(String templateName, M model, String outFilePath) throws IOException {
+    /**
+     * 加载数据
+     * @param data
+     * @return
+     */
+    public ExportWord loadData(Object data){
+        dataMap = new HashMap<String, Object>();
+        dataMap.put("data",data);
+        return this;
+    }
 
+    /**
+     * 导出文件
+     * @param outFilePath 自定义输出路径
+     * @return
+     */
+    public Boolean export(String outFilePath){
+        if(dataMap == null){
+            throw new NullPointerException("dataMap数据未加载!");
+        }
+        try(
+            FileOutputStream out = new FileOutputStream(outFilePath)
+        ) {
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(out,"UTF-8"));
+            template.process(dataMap, writer);
+            writer.flush();
+            return true;
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+
+    public InputStream getExportInputStream(String id){
         return null;
     }
 
